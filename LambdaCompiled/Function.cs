0203bbcc987ed43b6a727525c9b215f13550dd18
copperscreen <109984817+copperscreen@users.noreply.github.com>
2023-07-02 14:27:03 +0600

    aws lambda support

---
+++ LambdaCompiled/Function.cs
@@ -0,0 +1,24 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SwadeshComp;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public CompiledTable FunctionHandler(APIGatewayHttpApiV2ProxyRequest input, ILambdaContext context)
+    {
+        if (string.IsNullOrEmpty(input.Body)) return new CompiledTable();
+        return Scraper.Compiled(input.Body.Split(';'));
+    }
+}
